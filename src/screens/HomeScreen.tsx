a4a44c476d93f35e301eaa01b726cb9f62742a6d
Vicente Cruz <vcrb@uc.cl>
2023-12-30 13:13:35 -0300

    Feat - landing page without buttons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,32 @@
-import { Text, SafeAreaView } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Sizing, Typography } from '../styles';
 // import { Colors } from '../styles';
 
 export default function HomeScreen(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/images/logo.png')} />
+      <Text style={styles.header}>PiggyWallet</Text>
+      <Text style={styles.body}>Gestiona tus finanzas sabiamente</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  logo: {
+    width: Sizing.screen.width * 0.8,
+    height: Sizing.screen.width * 0.8,
+  },
+  header: {
+    ...Typography.headerStyles.large,
+    padding: Sizing.layout.x30,
+  },
+  body: {
+    ...Typography.bodyStyles.highlight,
+  },
+});
