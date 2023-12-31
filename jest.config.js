92180e178f88c603ee03dfac4e206407221d2c88
Vicente Cruz <vcrb@uc.cl>
2023-12-31 15:36:47 -0300

    Test - Managed to implement a dummy test

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
