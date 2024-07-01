d615f291183b306edb609fe9674c50e50bebc646
Vicente Cruz <vcrb@uc.cl>
2024-07-01 10:24:32 -0400

    fix - jest with cypress & linter finally working

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'cypress';
-import process from 'process'; // Add this line
 
 export default defineConfig({
   e2e: {
-    baseUrl:
-      process.env.EXPO_PUBLIC_APP_URL !== null
-        ? process.env.EXPO_PUBLIC_APP_URL
-        : 'http://localhost:8081',
+    baseUrl: 'http://localhost:8081',
     viewportWidth: 393,
     viewportHeight: 852,
     setupNodeEvents(on, config) {
