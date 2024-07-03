6df7fca89f5fc56b6328a532124842326f812e8e
Vicente Cruz <vcrb@uc.cl>
2024-07-03 16:06:45 -0400

    feat - Separated DebtTransaction in another component

--- src/screens/debtsStack/DebtDetailsScreen.tsx
+++ src/screens/debtsStack/DebtDetailsScreen.tsx
@@ -5,11 +5,9 @@ import {
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import type { Backend, Navigation } from '@/types';
+import type { Navigation } from '@/types';
 import { Colors, Sizing, Typography } from '@/styles';
 import useUserBalance from '@/hooks/debtsStack/useUserBalance';
 import useUserDebtsHistory from '@/hooks/debtsStack/useUserDebtsHistory';
@@ -17,6 +15,7 @@ import httpService from '@/service/api';
 import { END_POINT } from '@/service/constant';
 import UserBalance from '@/components/debtsStack/UserBalance';
 import ErrorText from '@/components/common/ErrorText';
+import DebtTransaction from '@/components/debtsStack/DebtTransaction';
 
 export default function DebtDetailsScreen({
   navigation,
@@ -78,36 +77,6 @@ export default function DebtDetailsScreen({
       ? `${route.params.debtorName} te debe $${balance}`
       : `Debes $${Math.abs(balance)} a ${route.params.debtorName}`;
 
-  const renderDebtTransactions = (
-    title: string,
-    transactions: Backend.DebtTransaction[]
-  ): JSX.Element => (
-    <View style={styles.section}>
-      <Text style={styles.sectionTitle}>{title}</Text>
-      {transactions.map((transaction) => (
-        <View key={transaction.id} style={styles.debtItem}>
-          <View style={styles.amountContainer}>
-            <View style={styles.amountAndDateContainer}>
-              <Text style={styles.amountText}>${transaction.amount.toLocaleString()}</Text>
-              <Text style={styles.dateText}>
-                {new Date(transaction.createdAt).toLocaleDateString()}
-              </Text>
-            </View>
-            <Text style={styles.descriptionText}>
-              {transaction?.description ?? 'Sin descripción'}
-            </Text>
-          </View>
-          <TouchableOpacity
-            style={styles.iconButton}
-            onPress={() => handleSettleDebtClick(transaction.id)}
-          >
-            <AntDesign name="check" size={36} color={Colors.palette.primary} />
-          </TouchableOpacity>
-        </View>
-      ))}
-    </View>
-  );
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.primaryText}>Tu saldo con {route.params.debtorName}</Text>
@@ -116,15 +85,28 @@ export default function DebtDetailsScreen({
         <UserBalance userBalance={userBalance} />
       </View>
       <ScrollView style={styles.debtsContainer}>
-        {userDebtsHistory?.presentWeek !== undefined &&
-          userDebtsHistory.presentWeek.length > 0 &&
-          renderDebtTransactions('Esta semana', userDebtsHistory.presentWeek)}
-        {userDebtsHistory?.lastWeek !== undefined &&
-          userDebtsHistory.lastWeek.length > 0 &&
-          renderDebtTransactions('Semana Pasada', userDebtsHistory.lastWeek)}
+        {userDebtsHistory?.presentWeek !== undefined && userDebtsHistory.presentWeek.length > 0 && (
+          <DebtTransaction
+            title="Esta semana"
+            transactions={userDebtsHistory.presentWeek}
+            onSettleDebtClick={handleSettleDebtClick}
+          />
+        )}
+        {userDebtsHistory?.lastWeek !== undefined && userDebtsHistory.lastWeek.length > 0 && (
+          <DebtTransaction
+            title="Semana Pasada"
+            transactions={userDebtsHistory.lastWeek}
+            onSettleDebtClick={handleSettleDebtClick}
+          />
+        )}
         {userDebtsHistory?.previousWeeks !== undefined &&
-          userDebtsHistory.previousWeeks.length > 0 &&
-          renderDebtTransactions('Semanas Anteriores', userDebtsHistory.previousWeeks)}
+          userDebtsHistory.previousWeeks.length > 0 && (
+            <DebtTransaction
+              title="Semanas Anteriores"
+              transactions={userDebtsHistory.previousWeeks}
+              onSettleDebtClick={handleSettleDebtClick}
+            />
+          )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -158,45 +140,7 @@ const styles = StyleSheet.create({
     ...Typography.bodyStyles.secondary,
   },
   debtsContainer: {
-    flex: 1,
     width: '100%',
-    padding: Sizing.x10,
-  },
-  section: {
-    marginVertical: Sizing.x20,
-  },
-  sectionTitle: {
-    ...Typography.subheaderStyles.muted,
-    marginBottom: Sizing.x10,
-  },
-  debtItem: {
-    flexDirection: 'row',
-    backgroundColor: Colors.palette.secondary,
-    padding: Sizing.x10,
-    marginBottom: Sizing.x10,
-    borderRadius: Sizing.x5,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  amountContainer: {
-    flex: 1,
-  },
-  amountAndDateContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  amountText: {
-    ...Typography.bodyStyles.secondary,
-    fontWeight: 'bold',
-  },
-  dateText: {
-    ...Typography.bodyStyles.primary,
-  },
-  descriptionText: {
-    ...Typography.bodyStyles.primary,
-  },
-  iconButton: {
     padding: Sizing.x10,
   },
 });
