fb3b036687a1fd1ad1574ea729d5faa09178d575
Vicente Cruz <vcrb@uc.cl>
2024-07-06 14:12:58 -0400

    fix - eliminated description from userExpenseType creation

--- src/screens/homeStack/UserExpenseTypeCreationScreen.tsx
+++ src/screens/homeStack/UserExpenseTypeCreationScreen.tsx
@@ -17,22 +17,15 @@ function UserExpenseTypeCreationScreen({
       .string()
       .required('Este campo es obligatorio')
       .test('len', 'El nombre no puede tener más de 70 caracteres', (str) => str.length <= 70),
-    description: yup
-      .string()
-      .test(
-        'len',
-        'La descripción no puede tener más de 255 caracteres',
-        (str) => str === undefined || str.length <= 255
-      ),
   });
 
   const [isSaving, setIsSaving] = useState(false);
 
-  const saveUserExpenseType = async (name: string, description?: string): Promise<void> => {
+  const saveUserExpenseType = async (name: string): Promise<void> => {
     setIsSaving(true);
 
     try {
-      await httpService.post(END_POINT.userExpenseTypes, { name, description });
+      await httpService.post(END_POINT.userExpenseTypes, { name });
       navigation.goBack();
     } catch (error) {
       Alert.alert('Error', 'Ha ocurrido un error, por favor intenta nuevamente más tarde');
@@ -45,9 +38,9 @@ function UserExpenseTypeCreationScreen({
     <View style={styles.container}>
       <Formik
         validationSchema={userExpenseTypeValidationSchema}
-        initialValues={{ name: '', description: '' }}
+        initialValues={{ name: '' }}
         onSubmit={async (values) => {
-          await saveUserExpenseType(values.name, values.description);
+          await saveUserExpenseType(values.name);
         }}
         validateOnMount
       >
@@ -61,16 +54,6 @@ function UserExpenseTypeCreationScreen({
               textContentType="none"
               autoCapitalize="none"
             />
-
-            <Field
-              component={CustomTextInput}
-              variant="primary"
-              name="description"
-              placeholder="Descripción del nuevo tipo de gasto"
-              textContentType="none"
-              autoCapitalize="none"
-            />
-
             {isSaving ? (
               <Button variant="fullWidth" loading />
             ) : (
